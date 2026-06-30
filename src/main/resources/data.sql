654f546f9f97abf122e401d46898649b8c8f8caf
fra.acosta@duocuc.cl <fra.acosta@duocuc.cl>
2026-06-30 04:21:33 -0400

    Agregar datos iniciales

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO usuarios (nombre, correo, edad)
+VALUES ('Francisca Acosta', 'francisca@duoc.cl', 22);
+
+INSERT INTO usuarios (nombre, correo, edad)
+VALUES ('Juan Pérez', 'juan@duoc.cl', 25);
+
+INSERT INTO usuarios (nombre, correo, edad)
+VALUES ('María González', 'maria@duoc.cl', 30);
